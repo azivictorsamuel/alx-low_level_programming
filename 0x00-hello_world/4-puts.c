1222deeed7a2569484d9c5b415ac389e2d771587
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-09-08 11:18:11 -0700

    my firt put code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Return: always zero
+ *
+ */
+int main(void)
+{
+	
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
