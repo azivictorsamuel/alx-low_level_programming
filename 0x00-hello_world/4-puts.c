93e8c63ed9574ed9e4e0bf88d1aae78a33f17e39
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-09-08 11:31:34 -0700

    my first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * main - entry point
- *
- * Return: always zero
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
