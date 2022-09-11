2b114276ca42ea8b2936c797a6f7cd1e0ac1bc1e
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-09-11 07:39:36 -0700

    reverse alphabeth

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+   * main - prints alphabet in reverse
+   *
+   * Return: zero
+   */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; 0 <= '10'; 1++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
