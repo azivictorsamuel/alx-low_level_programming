8cd0805aa797bfef047433c0523bc9bcfe89a77d
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-09-15 06:57:18 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,24 @@
 #included "main.h"
 
 /**
- * print_square - prints a square.
- *
- * @size: size of square
+ * print_square - check the code for Holberton School students.
+ * @size: Dimension of square wanted
+ * Return: Always 0.
  */
 void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size; j++)
-		{
-			_putchar('#');
-		}
-		if (i == size)
-		{
-			break;
-		}
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
