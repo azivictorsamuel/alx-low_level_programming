50e35a2868a30be0e86fa0d098d0887eaced5287
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-09-15 07:20:28 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,32 @@
-#include "main.h
-#lnclude <stdio.h>
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * main - main block, solve fizz buzz from numbers 1 to 100
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Multiples of both 3 and 5 should print FizzBuzz.
+ * Return: 0
  */
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 1; num < 101; num++)
+	i = 1;
+	while (i <= 100)
 	{
-		if (num % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if (num % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (num % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d", num);
+			printf("%d", i);
 
-		if (num != 100)
+		if (i != 100)
 			printf(" ");
+		i++;
 	}
 	printf("\n");
+
 	return (0);
 }
