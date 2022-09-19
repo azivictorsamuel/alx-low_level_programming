2f91f26d08f232637e433386a248f1b97fd24adb
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-09-19 14:10:47 -0700

    mfrom a bufer to another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,24 @@
 #include "main.h"
 
+#include "holberton.h"
+#include <stdio.h>
+
 /**
- * _strcpy - copies a string from one buffer to another
- *
- * @dest: destination buffer
- * @src: source string
- *
- * Return: pointer to dest
+ * *_strcpy - check the code for Holberton School students.
+ * @dest: Destination string
+ * @src: String to be copied from
+ * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
+	i = 0;
+	while (src[i] != 0)
+		i++;
+
+	for (j = 0; j <= i; j++)
+		dest[j] = src[j];
 
 	return (dest);
 }
