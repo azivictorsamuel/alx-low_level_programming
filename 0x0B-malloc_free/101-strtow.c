ad914884cdcba448c9199443a49ae19dc53212e6
azivictorsamuel <samuelvictorazi.sva@gmail.com>
2022-10-05 09:04:02 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,67 +3,52 @@
 #include <stdlib.h>
 
 /**
- * mem_clear - Frees memory
- * @s: String input for memory deallocation
- * Return: Nothing
- */
-void mem_clear(char **s)
-{
-  /* For loop variable*/
-	int i;
-
-	/* Loop through and free up each row of memory */
-	for (i = 0; s[i] != NULL; i++)
-	{
-		free(s[i]);
-	}
-	/* Free the block itself*/
-	free(s);
-}
-
-/**
- * word_count - Counts number of indexes in a string
- * @str: String being counted
- * Return: Number of indexes
- */
-int word_count(char *str)
-{
-  /* For loop variable. wc starts at 0 to ensure no garbage values are added */
-	int i;
-	int wc = 0;
-
-	/* Loop through given string */
-	for (i = 0; str[i]; i++)
-	{
-	  /* If the current index is anything other than a space, +1 to wc */
-		if (str[i] != ' ')
-		{
-			wc++;
-			/* Check the next index(es) for either space or null byte */
-			while (str[i] != ' ' && str[i] != '\0')
-				i++;
-			/* If the end of the string is reached, exclude the null byte itself by decrementing by 1 */
-			if (str[i] == '\0')
-				i--;
-		}
-	}
-
-	/* Return the word count*/
-	return (wc);
-}
-
-/**
- * strtow - "Master function": Splits a string such that each word gets its own line
- * @str: String input
- * Return: The pointer to the very first index of the list of words
+ * strtow - converts a string into its separate words
+ *
+ * @str: string to convert into words
+ *
+ * Return: 2d array pointer
  */
 char **strtow(char *str)
 {
-  /**
-   * Variables
-   * @i and @j: for loop counters and holding index placements
-   * @input_length: Holds length of the input string
-   * @word_size: Holds length of a particular word within the input string
-   * @array_row: Integer that keeps track of row index
-   * @array_column: Integer that keeps track of column index
+	char **ret, *ptr = str;
+	int wc = 0, i = 0;
 
+	if (str == 0 || *str == 0)
+		return (0);
+	while (*ptr)
+	{
+		if (!(*ptr == ' ') && (*(ptr + 1) == ' ' || *(ptr + 1) == 0))
+			wc++;
+		ptr++;
+	}
+	if (wc == 0)
+		return (NULL);
+	ret = malloc((wc + 1) * sizeof(char *));
+	if (ret == 0)
+		return (0);
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			for (ptr = str, wc = 0; *ptr != ' ' && *ptr != 0;)
+				wc++, ptr++;
+			ret[i] = malloc(wc + 1);
+			if (ret[i] == 0)
+			{
+				while (i >= 0)
+					free(ret[--i]);
+				free(ret);
+				return (0);
+			}
+			ptr = ret[i++];
+			while (*str != ' ' && *str != 0)
+				*ptr++ = *str++;
+			*ptr = 0;
+		}
+		else
+			str++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
